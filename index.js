25dcf3e486fb617654a4c9bfdf362916330bc0c8
Saiyalad <111510372+SaiyaladPS@users.noreply.github.com>
2023-04-05 04:53:54 +0700

    v2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const db = new sqlite3.Database("./database/JSPH.db");
 
-app.get('/users', (req, res) => {
+app.get('/', (req, res) => {
   db.all("SELECT * FROM JSPH", (err, data) => {
     if (err) {
       console.log(err)
@@ -33,30 +33,6 @@ app.post('/create', (req, res) => {
   });
 });
 
-app.post('/delete', (req, res) => {
-  const id = req.body.id;
-  db.run('DELETE FROM JSPH WHERE id =?', [id], (err, result)=> {
-    if (err) {
-      console.error(err.message);
-    } else {
-      res.send('Data deleted successfully');
-    }
-  })
-})
-
-app.post('/edit', (req, res) => {
-  const id = req.body.idE;
-  db.all('SELECT * FROM JSPH WHERE id = ?', [id], (err, row) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Internal server error');
-    } else if (!row) {
-      res.status(404).send('Record not found');
-    } else {
-      res.send(row);
-    }
-  });
-});
 
 const server = app.listen(3001, () => {
   console.log(`Server running on port ${server.address().port}`);
